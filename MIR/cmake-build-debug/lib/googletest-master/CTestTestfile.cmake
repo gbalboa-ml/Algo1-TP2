c0d4c9bc2c35a8eabf303de2fb379f509508c216
gbalboa <guillermo.balboa@mercadolibre.com>
2019-10-26 12:28:05 -0300

    Git ignore

--- MIR/cmake-build-debug/lib/googletest-master/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/gbalboa/IdeaProjects/Algo1-TP2/MIR/lib/googletest-master
-# Build directory: /Users/gbalboa/IdeaProjects/Algo1-TP2/MIR/cmake-build-debug/lib/googletest-master
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
