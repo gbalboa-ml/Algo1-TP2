9ac826fd8a835eaa596de6b2bb494104a665a840
gbalboa <guillermo.balboa@mercadolibre.com>
2019-11-02 16:11:32 -0300

    Otro test de limpiarAudio

--- MIR/tests/limpiarAudio.cpp
+++ MIR/tests/limpiarAudio.cpp
@@ -42,3 +42,16 @@ TEST(limpiarAudioTEST, audioConOutliers2){
     ASSERT_VECTOR(a, audioEsperado);
     ASSERT_VECTOR(outliers, outliersEsperado);
 }
+
+TEST(limpiarAudioTEST, audioConOutliers3){
+    audio a = {5,6,3,5,4,8,42,3,2,5,67,4,7,4,3,4,5,6,2,3,5,6,4,2,6};
+    audio outliers;
+    int profundidad = 6;
+    audio audioEsperado = {5,6,3,5,4,8,5,3,2,5,4,4,7,4,3,4,5,6,2,3,5,6,4,2,6};
+    audio outliersEsperado = {6,10};
+
+    limpiarAudio(a, profundidad, outliers);
+
+    ASSERT_VECTOR(a, audioEsperado);
+    ASSERT_VECTOR(outliers, outliersEsperado);
+}
